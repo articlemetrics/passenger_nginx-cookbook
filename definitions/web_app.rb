557b58a736f5ac6ff587857f340843163ee88f84
Martin Fenner <mfenner@plos.org>
2014-08-27 17:32:50 +0200

    correctly check for existence of directory

--- definitions/web_app.rb
+++ definitions/web_app.rb
@@ -1,16 +1,17 @@
 define :web_app, :template => 'web_app.conf.erb', :local => false, :enable => true do
 
   application_name = params[:name]
+  conf_dir = "#{node['nginx']['dir']}/conf.d"
 
   # create nginx directory if it doesn't exist, e.g. because nginx hasn't been installed yet
-  directory node['nginx']['dir'] do
+  directory conf_dir do
     owner 'root'
     group 'root'
     mode '0644'
     recursive true
   end
 
-  template "#{node['nginx']['dir']}/conf.d/#{application_name}.conf" do
+  template "#{conf_dir}/#{application_name}.conf" do
     source params[:template]
     local params[:local]
     owner 'root'
