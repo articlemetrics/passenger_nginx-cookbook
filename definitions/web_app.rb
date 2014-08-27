c6aaf9b65db0dc0924c9ee9058f7a87e9597a86b
Martin Fenner <mfenner@plos.org>
2014-08-27 17:45:13 +0200

    check that nginx is installed before creating web app folder

--- definitions/web_app.rb
+++ definitions/web_app.rb
@@ -1,17 +1,10 @@
 define :web_app, :template => 'web_app.conf.erb', :local => false, :enable => true do
 
   application_name = params[:name]
-  conf_dir = "#{node['nginx']['dir']}/conf.d"
 
-  # create nginx directory if it doesn't exist, e.g. because nginx hasn't been installed yet
-  directory conf_dir do
-    owner 'root'
-    group 'root'
-    mode '0644'
-    recursive true
-  end
+  include_recipe "passenger_nginx"
 
-  template "#{conf_dir}/#{application_name}.conf" do
+  template "#{node['nginx']['dir']}/conf.d/#{application_name}.conf" do
     source params[:template]
     local params[:local]
     owner 'root'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.1.6"
+version           "0.1.7"
 depends           "apt", "~> 2.5.3"
 depends           "build-essential", "2.0.0"
 depends           "ruby", "~> 0.1.0"
