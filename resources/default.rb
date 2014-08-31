2c80d68ef716c1ba4ce40916010c907e881b6c83
Martin Fenner <mfenner@plos.org>
2014-08-31 19:58:51 +0200

    use node['user'] by default

--- resources/default.rb
+++ resources/default.rb
@@ -3,6 +3,6 @@ default_action :config
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :rails_env, :kind_of => String, :default => "production"
-attribute :user, :kind_of => String, :default => "www-data"
+attribute :deploy_user, :kind_of => String, :default => node[:user]
 attribute :group, :kind_of => String, :default => "www-data"
 attribute :default_server, :kind_of => [TrueClass, FalseClass], :default => true
