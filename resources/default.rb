6b1d24822d9c84e9b3117e22a53cb23c891d89d2
Martin Fenner <mfenner@plos.org>
2014-09-03 11:33:37 +0200

    fixed typo in variable name

--- resources/default.rb
+++ resources/default.rb
@@ -2,7 +2,7 @@ actions :config, :cleanup
 default_action :config
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :rails_env, :kind_of => String, :default => node['ruby']['rails-env'] || "production"
-attribute :user, :kind_of => String, :default => node['ruby']['user'] || "www-data"
-attribute :group, :kind_of => String, :default => node['ruby']['group'] || "www-data"
+attribute :rails_env, :kind_of => String, :default => node['ruby']['rails_env']
+attribute :user, :kind_of => String, :default => node['ruby']['user']
+attribute :group, :kind_of => String, :default => node['ruby']['group']
 attribute :default_server, :kind_of => [TrueClass, FalseClass], :default => true
