7e0cacc2fd177650bc4f84d3542d654a9af0a62c
Martin Fenner <mfenner@plos.org>
2014-10-20 12:18:20 +0200

    use ENV variable DEPLOY_USER

--- resources/default.rb
+++ resources/default.rb
@@ -3,6 +3,6 @@ default_action :config
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :rails_env, :kind_of => String, :default => ENV['RAILS_ENV']
-attribute :user, :kind_of => String, :default => ENV['USER']
-attribute :group, :kind_of => String, :default => ENV['GROUP']
+attribute :user, :kind_of => String, :default => ENV['DEPLOY_USER']
+attribute :group, :kind_of => String, :default => ENV['DEPLOY_GROUP']
 attribute :default_server, :kind_of => [TrueClass, FalseClass], :default => true
