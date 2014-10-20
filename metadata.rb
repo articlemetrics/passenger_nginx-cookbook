1f91dcdd3ce78559ddb73e432a177af3e5e702e9
Martin Fenner <mfenner@plos.org>
2014-10-20 11:20:32 +0200

    use ENV variables

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.2.15"
+version           "0.3.0"
 depends           "apt", "~> 2.5.3"
-depends           "ruby", "~> 0.3.0"
+depends           "ruby", "~> 0.4.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
--- resources/default.rb
+++ resources/default.rb
@@ -2,7 +2,7 @@ actions :config, :cleanup
 default_action :config
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :rails_env, :kind_of => String, :default => node['ruby']['rails_env']
-attribute :user, :kind_of => String, :default => node['ruby']['user']
-attribute :group, :kind_of => String, :default => node['ruby']['group']
+attribute :rails_env, :kind_of => String, :default => ENV['RAILS_ENV']
+attribute :user, :kind_of => String, :default => ENV['USER']
+attribute :group, :kind_of => String, :default => ENV['GROUP']
 attribute :default_server, :kind_of => [TrueClass, FalseClass], :default => true
