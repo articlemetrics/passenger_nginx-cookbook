2a1654714bf612ee72a0e78557040eb442b3ee1f
Martin Fenner <mfenner@plos.org>
2014-10-21 14:30:39 +0200

    use chef variables for default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,7 @@ default['nginx']['log_dir'] = '/var/log/nginx'
 default['nginx']['worker_processes'] = 4
 default['nginx']['worker_connections'] = 768
 default['nginx']['default_site_enabled'] = false
+
+default['ruby']['rails_env'] = "development"
+default['ruby']['deploy_user'] = "vagrant"
+default['ruby']['deploy_group'] = "vagrant"
--- resources/default.rb
+++ resources/default.rb
@@ -2,7 +2,7 @@ actions :config, :cleanup
 default_action :config
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :rails_env, :kind_of => String, :default => ENV['RAILS_ENV']
-attribute :user, :kind_of => String, :default => ENV['DEPLOY_USER']
-attribute :group, :kind_of => String, :default => ENV['DEPLOY_GROUP']
+attribute :rails_env, :kind_of => String, :default => node['ruby']['rails_env']
+attribute :user, :kind_of => String, :default => node['ruby']['deploy_user']
+attribute :group, :kind_of => String, :default => node['ruby']['deploy_group']
 attribute :default_server, :kind_of => [TrueClass, FalseClass], :default => true
