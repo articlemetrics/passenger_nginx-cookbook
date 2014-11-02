55693399872a666da0b7e9dca4bb9ab4f8b5f2e9
Martin Fenner <mfenner@plos.org>
2014-11-02 16:18:29 +0100

    update dependency on ruby cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.2"
+version           "0.3.3"
 depends           "apt", "~> 2.5.3"
-depends           "ruby", "~> 0.4.0"
+depends           "ruby", "~> 0.5.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
