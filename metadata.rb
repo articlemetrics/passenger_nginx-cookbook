62fc6aab4aba4b4ed7274e7f68517af950417c35
Martin Fenner <mfenner@plos.org>
2014-11-28 18:12:57 +0100

    loosen dependency on apt cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.4"
-depends           "apt", "~> 2.6.0"
+version           "0.3.6"
+depends           "apt"
 depends           "ruby", "~> 0.6.0"
 
 %w{ ubuntu }.each do |platform|
