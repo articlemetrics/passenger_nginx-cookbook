982440c212cfc6f7e282efe1377df5890f57f5c2
Martin Fenner <mfenner@plos.org>
2015-01-08 10:08:27 +0100

    symlink current folder

--- providers/default.rb
+++ providers/default.rb
@@ -24,13 +24,18 @@ action :config do
   end
 
   # create application root folder and set permissions
-  %W{ #{new_resource.name} #{new_resource.name}/current #{new_resource.name}/current/public }.each do |dir|
+  %W{ #{new_resource.name} #{new_resource.name}/shared #{new_resource.name}/shared/public }.each do |dir|
     directory "/var/www/#{dir}" do
       owner new_resource.user
       group new_resource.group
       mode '0755'
       recursive true
     end
+  end
+
+  # we symlink from the shared folder instead of creating the root folder directly
+  link "/var/www/#{new_resource.name}/current" do
+    to "/var/www/#{new_resource.name}/shared"
   end
 
   template "#{node['nginx']['dir']}/sites-enabled/#{new_resource.name}.conf" do
