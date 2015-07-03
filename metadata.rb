98fca4bcd2d5d8ce42c38f700d0ffd84d149b9c2
Martin Fenner <mfenner@plos.org>
2015-07-02 23:51:49 -0700

    install nodejs via ruby cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email  "mfenner@plos.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.1"
+version           "0.5.2"
 depends           "apt"
-depends           "ruby", "~> 0.6.0"
+depends           "ruby", "~> 0.7.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
