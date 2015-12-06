68577e473e4e8e61b945f6b3687962476803a159
Martin Fenner <martin.fenner@datacite.org>
2015-12-06 18:22:43 -0500

    require dotenv and capistrano cookbooks

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,19 @@
 name              "passenger_nginx"
 maintainer        "Martin Fenner"
-maintainer_email  "mfenner@plos.org"
+maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.5.4"
+version           "0.6.0"
+
+# opscode cookbooks
 depends           "apt"
+depends           "nodejs"
+
+# our own cookbooks
 depends           "ruby", "~> 0.7.0"
+depends           "dotenv", "~> 0.2.0"
+depends           "capistrano", "~> 0.10.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
