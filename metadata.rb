2bef6f8c797c58f7c011cd154aecb185b733b0b2
Martin Fenner <martin.fenner@datacite.org>
2015-12-07 12:06:05 -0500

    use correct PPA for passenger

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.6.4"
+version           "0.6.5"
 
 # opscode cookbooks
 depends           "apt"
@@ -12,7 +12,6 @@ depends           "nodejs"
 
 # our own cookbooks
 depends           "ruby", "~> 0.7.0"
-depends           "dotenv", "~> 0.2.0"
 
 %w{ ubuntu }.each do |platform|
   supports platform
