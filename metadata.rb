ece479ac320e9f0f716a63a2034462f2682e0b3a
Martin Fenner <martin.fenner@datacite.org>
2015-12-09 18:33:52 -0500

    don't use capistrano folders

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.6.6"
+version           "1.0.0"
 
 # opscode cookbooks
 depends           "apt"
