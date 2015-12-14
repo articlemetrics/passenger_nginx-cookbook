f1694e91ea8910f79d8333223e7c813fcac4f161
Martin Fenner <martin.fenner@datacite.org>
2015-12-14 14:59:30 +0100

    syslog server as chef attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -13,5 +13,6 @@ default['ruby']['merge_slashes_off'] = true
 default['ruby']['api_only'] = false
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev python-software-properties software-properties-common zlib1g-dev }
 default['ruby']['packages'] += %w{ avahi-daemon libnss-mdns } if node['ruby']['rails_env'] != "production"
+default['ruby']['syslog_server'] = node['fqdn']
 
 default['nodejs']['repo'] = 'https://deb.nodesource.com/node_0.12'
