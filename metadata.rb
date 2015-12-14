c545d276343f29757a11657ef7910b3561c9c86d
Martin Fenner <martin.fenner@datacite.org>
2015-12-14 20:01:07 +0100

    use forked rsyslog cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.8"
+version           "1.0.9"
 
 # opscode cookbooks
 depends           "apt"
