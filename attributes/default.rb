8761537ca5e6814e34540bf5b4ed0769a94207ed
Martin Fenner <martin.fenner@datacite.org>
2015-12-14 19:14:55 +0100

    simplify configuration of rsyslog server

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@ default['nginx']['log_dir'] = '/var/log/nginx'
 default['nginx']['worker_processes'] = 4
 default['nginx']['worker_connections'] = 768
 default['nginx']['default_site_enabled'] = false
+default['nginx']['rsyslog_server'] = '127.0.0.1'
 
 default['ruby']['rails_env'] = "development"
 default['ruby']['deploy_user'] = "vagrant"
@@ -13,6 +14,5 @@ default['ruby']['merge_slashes_off'] = true
 default['ruby']['api_only'] = false
 default['ruby']['packages'] = %w{ curl git libmysqlclient-dev python-software-properties software-properties-common zlib1g-dev }
 default['ruby']['packages'] += %w{ avahi-daemon libnss-mdns } if node['ruby']['rails_env'] != "production"
-default['rsyslog']['server'] = true
 
 default['nodejs']['repo'] = 'https://deb.nodesource.com/node_0.12'
