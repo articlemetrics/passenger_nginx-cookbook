a6861bfa9dc7dc01793d854d4cb057772cfedefb
Martin Fenner <martin.fenner@datacite.org>
2015-12-15 19:28:04 +0100

    log correct ip via proxy

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ maintainer_email  "mfenner@datacite.org"
 license           "Apache 2.0"
 description       "Configure nginx with passenger"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.9"
+version           "1.0.10"
 
 # opscode cookbooks
 depends           "apt"
 depends           "nodejs"
+depends           "hostnames"
 
 # our own cookbooks
 depends           "ruby", "~> 0.7.0"
